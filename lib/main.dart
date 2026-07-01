845f568d1e9d5a42437729f7ab96eaeb8f7cfef1
Paula Cecilia <paulacecilia4700@gmail.com>
2026-07-01 18:20:52 -0300

    Adiciona botão de nova frase

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,14 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-
+title: "frases do dia",
+    home: Container(
+      color: Colors.white,
+      child: Column(
+        children: [
+          Text("paula cecilia longhi")
+        ],
+      ),
+    ),
   ));
 }
